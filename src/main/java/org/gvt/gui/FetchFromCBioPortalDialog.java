a01ae7e6b51c039cc3bd5d0a668c223f5f7dcac2
B. Arman Aksoy <arman@aksoy.org>
2012-05-30 18:30:03 -0400

    first steps for cBio Portal accessor implementation. Not complete yet, but close.

---
+++ src/main/java/org/gvt/gui/FetchFromCBioPortalDialog.java
@@ -0,0 +1,224 @@
+package org.gvt.gui;
+
+import org.biopax.paxtools.causality.data.CBioPortalAccessor;
+import org.biopax.paxtools.causality.data.CancerStudy;
+import org.biopax.paxtools.causality.data.CaseList;
+import org.biopax.paxtools.causality.data.GeneticProfile;
+import org.biopax.paxtools.causality.util.HGNCUtil;
+import org.biopax.paxtools.model.BioPAXElement;
+import org.biopax.paxtools.model.Model;
+import org.biopax.paxtools.model.level3.ProteinReference;
+import org.biopax.paxtools.model.level3.RelationshipXref;
+import org.biopax.paxtools.model.level3.Xref;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.widgets.List;
+import org.gvt.ChisioMain;
+import org.patika.mada.dataXML.ChisioExperimentData;
+
+import java.io.IOException;
+import java.util.*;
+
+public class FetchFromCBioPortalDialog extends Dialog {
+    private ChisioMain main;
+    private Shell shell;
+    private CBioPortalAccessor cBioPortalAccessor = null;
+
+    public FetchFromCBioPortalDialog(ChisioMain main) {
+        super(main.getShell(), SWT.NONE);
+        this.main = main;
+    }
+
+    public void open() {
+        if(cBioPortalAccessor == null) {
+            try {
+                cBioPortalAccessor = new CBioPortalAccessor();
+            } catch (IOException e) {
+                MessageDialog.openError(main.getShell(),
+                        "Error!",
+                        "Could not access to cBio Portal.\n" + e.toString()
+                );
+
+                return;
+            }
+        }
+        createContents();
+
+   		shell.pack();
+   		shell.setLocation(
+   			getParent().getLocation().x + (getParent().getSize().x / 2) -
+   				(shell.getSize().x / 2),
+   			getParent().getLocation().y + (getParent().getSize().y / 2) -
+   				(shell.getSize().y / 2));
+
+   		shell.open();
+
+   		Display display = getParent().getDisplay();
+   		while (!shell.isDisposed()) {
+   			if (!display.readAndDispatch()) display.sleep();
+   		}
+   	}
+
+    private void createContents() {
+        shell = new Shell(getParent(), SWT.RESIZE | SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+        shell.setText("Fetch from cBio Portal");
+
+        ImageDescriptor id = ImageDescriptor.createFromFile(
+                ChisioMain.class, "icon/cbe-icon.png");
+        shell.setImage(id.createImage());
+
+        GridLayout gridLayout = new GridLayout();
+        gridLayout.numColumns = 2;
+        shell.setLayout(gridLayout);
+
+        GridData gridData;
+
+        Label cancerStudyLabel = new Label(shell, SWT.BOLD);
+        cancerStudyLabel.setText("1) Select a cancer study");
+        gridData = new GridData(GridData.FILL, GridData.FILL, false, false);
+        gridData.horizontalSpan = 2;
+        cancerStudyLabel.setLayoutData(gridData);
+
+        final Combo comboDropDown = new Combo(shell, SWT.DROP_DOWN | SWT.BORDER | SWT.READ_ONLY);
+        gridData = new GridData(GridData.FILL, GridData.FILL, true, true);
+        gridData.horizontalSpan = 2;
+        comboDropDown.setLayoutData(gridData);
+
+        comboDropDown.removeAll();
+        for (CancerStudy cancerStudy : cBioPortalAccessor.getCancerStudies()) {
+            comboDropDown.add(cancerStudy.getName());
+        }
+
+        Label caseListLabel = new Label(shell, SWT.NONE);
+        caseListLabel.setText("2) Select a case list");
+        gridData = new GridData(GridData.FILL, GridData.FILL, false, false);
+        gridData.horizontalSpan = 2;
+        caseListLabel.setLayoutData(gridData);
+
+        final Combo caseListList = new Combo(shell, SWT.DROP_DOWN | SWT.BORDER | SWT.READ_ONLY);
+        gridData = new GridData(GridData.FILL, GridData.FILL, true, true);
+        gridData.horizontalSpan = 2;
+        caseListList.setLayoutData(gridData);
+
+        Label genomicProfileLabel = new Label(shell, SWT.NONE);
+        genomicProfileLabel.setText("3) Select a genomic profile");
+        gridData = new GridData(GridData.FILL, GridData.FILL, false, false);
+        gridData.horizontalSpan = 2;
+        genomicProfileLabel.setLayoutData(gridData);
+
+        final List genomicProfilesList = new List(shell, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI);
+        gridData = new GridData(GridData.FILL, GridData.FILL, true, true);
+        gridData.horizontalSpan = 2;
+        gridData.heightHint = 100;
+        genomicProfilesList.setLayoutData(gridData);
+
+        final Button fetchButton = new Button(shell, SWT.NONE);
+        fetchButton.setText("Load data");
+        gridData = new GridData(GridData.END, GridData.CENTER, true, false);
+        fetchButton.setEnabled(false);
+        fetchButton.setLayoutData(gridData);
+
+        Button cancelButton = new Button(shell, SWT.NONE);
+        cancelButton.setText("Cancel");
+        gridData = new GridData(GridData.BEGINNING, GridData.CENTER, true, false);
+        cancelButton.setLayoutData(gridData);
+
+        comboDropDown.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetDefaultSelected(SelectionEvent selectionEvent) {
+                widgetSelected(selectionEvent);
+            }
+
+            @Override
+            public void widgetSelected(SelectionEvent selectionEvent) {
+                CancerStudy cancerStudy = cBioPortalAccessor.getCancerStudies().get(comboDropDown.getSelectionIndex());
+                cBioPortalAccessor.setCurrentCancerStudy(cancerStudy);
+                try {
+                    caseListList.removeAll();
+                    for (CaseList caseList : cBioPortalAccessor.getCaseListsForCurrentStudy()) {
+                        caseListList.add(caseList.getDescription());
+                    }
+                    caseListList.select(0);
+
+                    genomicProfilesList.removeAll();
+                    for (GeneticProfile geneticProfile : cBioPortalAccessor.getGeneticProfilesForCurrentStudy()) {
+                        genomicProfilesList.add(geneticProfile.getName());
+                    }
+
+                } catch (IOException e) {
+                    MessageDialog.openError(
+                            main.getShell(),
+                            "Error!",
+                            "Could not load meta data for current study.\n" + e.toString()
+                    );
+
+                }
+
+            }
+        });
+
+        genomicProfilesList.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent selectionEvent) {
+                fetchButton.setEnabled(genomicProfilesList.getSelectionCount() > 0);
+            }
+        });
+
+        fetchButton.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent selectionEvent) {
+                try {
+                    int selectionIndex = caseListList.getSelectionIndex();
+                    CaseList caseList = cBioPortalAccessor.getCaseListsForCurrentStudy().get(selectionIndex);
+                    cBioPortalAccessor.setCurrentCaseList(caseList);
+                } catch (IOException e) {
+                    MessageDialog.openError(
+                            main.getShell(),
+                            "Error!",
+                            "Could not load case lists for current study.\n" + e.toString()
+                    );
+
+                    return;
+                }
+
+                java.util.List<GeneticProfile> geneticProfiles = null;
+                try {
+                    geneticProfiles = cBioPortalAccessor.getGeneticProfilesForCurrentStudy();
+                } catch (IOException e) {
+                    MessageDialog.openError(
+                            main.getShell(),
+                            "Error!",
+                            "Could not load case lists for current study.\n" + e.toString()
+                    );
+
+                    return;
+                }
+
+                java.util.List<String> geneNames = new ArrayList<String>();
+                Model model = main.getOwlModel();
+                for (RelationshipXref xref : model.getObjects(RelationshipXref.class)) {
+                    if(xref.getDb().startsWith("HGNC"))
+                        geneNames.add(HGNCUtil.getSymbol(Integer.parseInt(xref.getId().split("%3A")[1])));
+                }
+
+                for (int i : genomicProfilesList.getSelectionIndices()) {
+                    GeneticProfile geneticProfile = geneticProfiles.get(i);
+                    cBioPortalAccessor.setCurrentGeneticProfiles(Collections.singletonList(geneticProfile));
+                }
+                shell.close();
+            }
+        });
+
+        cancelButton.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent selectionEvent) {
+                shell.close();
+            }
+        });
+    }
+}
